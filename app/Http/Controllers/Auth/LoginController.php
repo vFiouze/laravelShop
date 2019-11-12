bd5a51e9bd668fe831ed1ad7a04d35146b65c582
Vincent Fuseau <vFiouze@MacBook-Pro-de-Vincent.local>
2019-11-11 18:28:39 -0800

    delete event fire

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -48,7 +48,8 @@ class LoginController extends Controller
         ]);
 
         //fire new event at login to send email
-        event(new CustomerLoginEvent($user));
+        //delete event
+        //event(new CustomerLoginEvent($user));
 
     }
 }
